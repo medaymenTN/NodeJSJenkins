21ea331f49808a4cce07463ab334df901838eab3
medaymenTN <mohamedaymen.ourabi@esprit.tn>
2018-10-08 17:59:19 -0700

    update server.js

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ var app = express();
 app.get('/',function(req,res) {
   res.send("Hello World");
 });
+
+// route that render message 
 app.get('/message',(req,res)=>{
 
     res.send("hello jenkins")
 })
+
 
 app.listen(4000);
